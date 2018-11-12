787dd374a3cdd84b56b0f8414da24eadb83d1b09
Marsh Macy <marsma@microsoft.com>
2018-11-12 09:56:14 -0800

    fix code block (indent)

--- azure/cosmos/cosmos_client.py
+++ azure/cosmos/cosmos_client.py
@@ -52,23 +52,23 @@ class CosmosClient(object):
 
         .. code-block:: python
 
-        import azure.cosmos.cosmos_client as cosmos_client
+            import azure.cosmos.cosmos_client as cosmos_client
 
-        config = {
-            'ENDPOINT': 'FILLME',
-            'PRIMARYKEY': 'FILLME',
-            'DATABASE': 'CosmosDatabase',
-            'CONTAINER': 'CosmosContainer'
-        }
+            config = {
+                'ENDPOINT': 'FILLME',
+                'PRIMARYKEY': 'FILLME',
+                'DATABASE': 'CosmosDatabase',
+                'CONTAINER': 'CosmosContainer'
+            }
 
-        # Initialize the Cosmos client
-        client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
-                                            'masterKey': config['PRIMARYKEY']})
-        # Create a database
-        db = client.CreateDatabase({'id': config['DATABASE']})
+            # Initialize the Cosmos client
+            client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
+                                                'masterKey': config['PRIMARYKEY']})
+            # Create a database
+            db = client.CreateDatabase({'id': config['DATABASE']})
 
-        # Create a container
-        container = client.CreateContainer(db['_self'], container_definition, options)
+            # Create a container
+            container = client.CreateContainer(db['_self'], container_definition, options)
 
     :param str url_connection:
         The URL for connecting to the DB server.
